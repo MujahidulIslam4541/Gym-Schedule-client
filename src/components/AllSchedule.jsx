f78d74b0fd51ba4571d3492dbbd129ee365c7fcc
MujahidulIslam4541 <mirifat4541@gmail.com>
2024-12-04 04:19:00 +0600

    data deleted section added

--- src/components/AllSchedule.jsx
+++ src/components/AllSchedule.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { FaEdit, FaEyeSlash } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const AllSchedule = () => {
-  const allSchedule = useLoaderData();
-  const { title, date, day, time } = allSchedule;
+  const allScheduleLoader = useLoaderData();
+  const [allSchedule, setAllSchedule] = useState(allScheduleLoader);
+  //   const { title, date, day, time } = allSchedule;
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    })
+    fetch(`http://localhost:5000/schedule/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        const filterData = allSchedule.filter(
+          (schedule) => id !== schedule._id
+        );
+        setAllSchedule(filterData);
+        if (result.deletedCount) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+      });
+  };
+
   return (
     <div>
       <div className="flex flex-col justify-center items-center space-y-10 mt-10">
@@ -40,15 +73,24 @@ const AllSchedule = () => {
               ) : (
                 allSchedule.map((schedule, index) => (
                   <tr key={schedule._id}>
-                    <th>{index+1}</th>
+                    <th>{index + 1}</th>
                     <td>{schedule.title}</td>
                     <td>{schedule.day}</td>
                     <td>{schedule.date}</td>
                     <td>{schedule.time}</td>
                     <div className="flex gap-3 text-xl">
-                      <p className="text-pink-500"><FaEdit/></p>
-                      <p className="text-pink-500"><MdDelete /></p>
-                      <p className="text-pink-500"><FaEyeSlash /></p>
+                      <p className="text-pink-500">
+                        <FaEdit />
+                      </p>
+                      <p
+                        onClick={() => handleDelete(schedule._id)}
+                        className="text-pink-500"
+                      >
+                        <MdDelete />
+                      </p>
+                      <p className="text-pink-500">
+                        <FaEyeSlash />
+                      </p>
                     </div>
                   </tr>
                 ))
